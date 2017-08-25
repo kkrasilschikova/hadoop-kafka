2505997fd36558e4e577e66aa60037159dec4dd3
kkrasilschikova <kseniya.krasilschikova@gmail.com>
2017-08-25 19:49:47 +0300

    modified regex

--- src/main/scala/kafka/Processing.scala
+++ src/main/scala/kafka/Processing.scala
@@ -8,11 +8,11 @@ import scala.sys.process._
 import scala.util.matching.Regex
 
 class Processing {
-  val http: Regex="^https?://[0-9a-z]+(.*)[0-9a-z]+(.*)[a-z]+(.*)[a-z]+:\\d{1,}/[a-z]+/[a-z0-9]+/[a-z]+/\\d{8}/[0-9-_a-zA-Z]+(.*)zip".r
-  val archive: Regex="[0-9-_a-zA-Z]+(.*)zip".r
+  val http: Regex="^https?://[0-9a-z]+(.?)[0-9a-z]+(.?)[a-z]+(.?)[a-z]+:\\d{1,}/[a-z]+/[a-z0-9]+/[a-z]+/\\d{8}/[0-9-_a-zA-Z]+(.?)zip".r
+  val archive: Regex="[0-9-_a-zA-Z]+(.?)zip".r
 
   def validated(uri: URI,
-                pattern: String="^https?://[0-9a-z]+(.*)[0-9a-z]+(.*)[a-z]+(.*)[a-z]+:\\d{1,}/[a-z]+/[a-z0-9]+/[a-z]+/\\d{8}/[0-9-_a-zA-Z]+(.*)zip\\?op=OPEN$"): Boolean={
+                pattern: String="^https?://[0-9a-z]+(.*)[0-9a-z]+(.?)[a-z]+(.?)[a-z]+:\\d{1,}/[a-z]+/[a-z0-9]+/[a-z]+/\\d{8}/[0-9-_a-zA-Z]+(.?)zip\\?op=OPEN$"): Boolean={
     uri.uri.matches(pattern)
   }
 
